18ec09a1391e053f85e5354567b5b900b970a28c
JILS PATEL <100358865+JILSPATEL@users.noreply.github.com>
2024-03-07 22:21:26 +0530

    add Average_of_Array.py

---
+++ TCS_NQT/Average_of_Array.py
@@ -0,0 +1,41 @@
+'''
+
+Problem Statement: Given an array, we have to find the average of all the elements in the array.
+
+Examples:
+
+Example 1:
+Input: N = 5, array[] = {1,2,3,4,5}
+Output: 3
+Explanation: 
+Average is the sum of all the elements divided by number of elements.
+Therefore (1+2+3+4+5)/5 = 3.
+
+Example 2:
+Input:  N=6, array[] = {1,2,1,1,5,1}
+Output: 1.8
+Explanation: 
+Average is the sum of all the elements divided by number of elements.
+Therefore (1+2+1+1+5+1)/6 = 1.8
+
+'''
+
+
+def calculate_average_of_array(array, size):
+    array_sum = 0
+    for i in range(size):
+        array_sum += array[i]
+
+    average = round(array_sum / size, 1)
+    return average
+
+
+size = int(input("How many elements do you want to enter: "))
+array = []
+for i in range(size):
+    element = int(input(f"Enter the {i+1} number: "))
+    array.append(element)
+
+print("Your input array is:", array)
+
+print("Average of the array is:", calculate_average_of_array(array, size))

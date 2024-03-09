984f5881b70c6c693a5f4d0054d195e41dfd3ab7
JILS PATEL <100358865+JILSPATEL@users.noreply.github.com>
2024-03-09 22:27:32 +0530

    FindRepeatingElement.py

--- TCS_NQT/FindRepeatingElement.py
+++ TCS_NQT/FindRepeatingElement.py
@@ -20,21 +20,45 @@ Output:
 Explanation:
  Only 1 is occurring more than once in the given array.
 
+
+Problem Statement: Find all the non-repeating elements for a given array. Outputs can be in any order.
+
+Examples:
+
+Example 1:
+Input:
+ Nums = [1,2,-1,1,3,1]
+Output:
+ 2,-1,3
+Explanation:
+ 1 is the only element in the given array which occurs thrice in the array. -1,2,3 occurs only once and hence, these are non-repeating elements of the given array.
+
+Example 2:
+Input:
+ Nums = [1,2,3]
+Output:
+ 1,2,3
+Explanation:
+ All elements present in the array occur once. Hence, every element is non-repeating.
 """
 
 
-def repeateEle(array):
-    repeated_elements = []
+def find_repeating_elements(array):
+    repeating_elements = []
     unique_elements = []
 
     for element in array:
         if element in unique_elements:
-            repeated_elements.append(element)
+            repeating_elements.append(element)
         else:
             unique_elements.append(element)
-
-    return sorted(repeated_elements)
-
+    
+    all_elements = set(array)
+    repeating_set = set(repeating_elements)
+    non_repeating_set = all_elements - repeating_set
+    
+    print("Repeating elements of the array are:", sorted(list(repeating_set)))
+    print("Non-repeating elements of the array are:", sorted(list(non_repeating_set)))
 
 size = int(input("How many elements do you want to enter: "))
 array = []
@@ -43,4 +67,4 @@ for i in range(size):
     array.append(element)
 
 print("Your input array is:", array)
-print("Repeating elements of the array are:", repeateEle(array))
+find_repeating_elements(array)
